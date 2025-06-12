2d3bf505e44065506ff5737ff1c8ca7191a84a9a
Your Name <you@example.com>
2025-06-12 13:09:22 +0900

    4교시 입니다

--- 4_Machine.py
+++ 4_Machine.py
@@ -1,10 +1,88 @@
 #min 평균값
 import numpy
+import matplotlib.pyplot as plt
+from scipy import stats
+
+#10년된 자동차의 속도를 예측합니다
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+def myfunc(x): return slope * x + intercept
+
+#속도
+speed = myfunc(20)
+print(speed)
+
+#선형 회귀 Linear Regression 관계를 찾을때 회귀라는 용어를 사용하는데
+'''
+머신러닝과 통계 모델링에서는 이러한 관계를 사용하여 미래 이벤트의 결과를 예측합니다
+'''
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+#scipy 선형 회귀선을 가져와 그립니다
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+#주어진 x값에 대해서 선형회귀 직선을 기반으로 y값을 예측합니다
+def myfunc(x): return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+
+plt.scatter(x,y)
+plt.plot(x, mymodel)
+plt.show()
+'''
+slope : 회귀선의 기울기
+intercept : 회귀선이 y축과 만나는 지점
+r : 피어슨 상관계수(두 변수간의 선형 상관 정도 -1 ~1)
+p : 기울기의 유의확률 0에 가까울수록 통계적으로 유희함
+std_err : 표준오차 (기울기의 추정치의 표준오차: 작을수록 신뢰도가 높다)
+'''
+
+plt.scatter(x,y)
+plt.show()
+
+
+#무작위 데이터분포 첫번재 배열은 평균을 5 표준편차를 1 두번째 배열은 평균을 10으로 표준편차를 2로 만듭니다
+x = numpy.random.normal(5.0, 1.0, 1000)
+y = numpy.random.normal(10.0, 2.0, 1000)
+
+plt.scatter(x,y)
+plt.show()
+
+
+#pip install matplotlib
+#정규 데이터 분포 Normal Data Distribution => 주어진 값을 중심으로 값이 집중된 배열
+#확률론에서 이러한 종류의 데이터 분포는 정규데이터 분포 또는 가우스데이터 분포라 합니다
+x = numpy.random.normal(5.0, 1.0, 100000)
+#평균값을 5.0 표준편차를 1.0 값이 4 ~6사이 있음 최고값은 약 5.0
+plt.hist(x, 100)
+plt.show()
+
+
+#20250612 데이터분포 Data Distribution
+# 빅데이터 세트를 어떻게 얻을수 있나요 ? 아래에 0 ~5 사이의 난수 250개를 포함하는 배열을 만듭니다
+x = numpy.random.uniform(0.0, 5.0, 250)
+
+plt.hist(x, 5)
+plt.show()
+print(x)
+
+x = numpy.random.uniform(0.0, 5.0, 100000)
+
+plt.hist(x, 100)
+plt.show()
+print(x)
+
+
 
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
 x = numpy.mean(speed)
 print(x)
+
+#머리가 아프다 시각화
 
 #중앙값은 모든 값을 정렬한후 중앙에 있는 값 입니다 median()
 #가운데에 숫자가 2개 있을경우 /2를 사용합니다
@@ -13,7 +91,7 @@ print(x)
 
 #일반적인 값이 mode 가장 많이 나타나는 값 Scipy [넘파이를 기반으로 하는 과학적 파이선라이브러리]
 #pip install scipy
-from scipy import stats
+
 
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 x = stats.mode(speed)
